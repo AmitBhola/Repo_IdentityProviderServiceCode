7fa43199afefe129a417c820ca64a70345eb0398
amitbhola03 <amitbhola03@gmail.com>
2015-04-17 17:25:06 -0500

    Added end to end encryption for the get data function

--- IdentityProviderService/Service1.svc.cs
+++ IdentityProviderService/Service1.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using EncryptionAndEncodingHelper;
 
 namespace IdentityProviderService
 {
@@ -12,9 +13,12 @@ namespace IdentityProviderService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
+        private EncryptionAndEncoding eeh = new EncryptionAndEncoding("0123456789ABCDEF", "ABCDEFGH");
         public string GetData(String value)
         {
-            return string.Format("You entered: {0}", value);
+            string username = eeh.DecryptAndDecodeText(value).Split(':')[0];
+            string password = eeh.DecryptAndDecodeText(value).Split(':')[1];
+            return eeh.EncryptAndEncodeText(string.Format("The Password you entered: {0}", password));
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
